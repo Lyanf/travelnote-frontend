b11c4f4d747e85e6c2a096b817dc99928a4fc0c4
lyf <609136080@qq.com>
2019-12-20 11:39:12 +0800

    add search component

---
+++ src/components/ArticleSearch/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <el-card>
+    <div slot="header">
+      <span>条件检索</span>
+    </div>
+    <el-row gutter="50">
+      <el-col span="5">
+        <el-input v-model="header" placeholder="请输入标题" />
+      </el-col>
+      <el-col span="5">
+        <el-input v-model="name" placeholder="作者" />
+      </el-col>
+      <el-col span="10">
+        <el-date-picker
+          v-model="date"
+          type="daterange"
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        />
+      </el-col>
+      <el-col span="4">
+        <el-button type="primary" @click="searchClicked">检索</el-button>
+      </el-col>
+    </el-row>
+  </el-card>
+</template>
+<script>
+var dateFormat = require('dateformat')
+export default {
+  name: 'ArticleSearch',
+  data: function() {
+    return {
+      header: '',
+      name: '',
+      date: ''
+    }
+  },
+  methods: {
+    searchClicked() {
+      const obj = {}
+      const t = {}
+      t['header'] = this.header
+      t['name'] = this.name
+      t['date'] = this.date
+      for (const i in t) {
+        if (t[i] !== '' && t[i] !== null) {
+          obj[i] = t[i]
+        }
+      }
+      if (t.hasOwnProperty('date')) {
+        const date1 = dateFormat(t['date'][0], 'yyyy,mm,dd')
+        const date2 = dateFormat(t['date'][1], 'yyyy,mm,dd')
+        t['date'][0] = date1
+        t['date'][1] = date2
+      }
+      this.$emit('searchEvent', obj)
+    }
+  }
+}
+</script>
--- src/views/dataexport/index.vue
+++ src/views/dataexport/index.vue
@@ -1,13 +1,24 @@
 <template>
-
+  <ArticleSearch @searchEvent="filter" />
 </template>
 
 <script>
-  export default {
-    name: 'index'
+import ArticleSearch from '@/components/ArticleSearch/index'
+export default {
+  name: 'Index',
+  components: { ArticleSearch },
+  methods: {
+    filter(filterObj) {
+      console.log(filterObj)
+    }
   }
+}
 </script>
 
 <style scoped>
-
+.el-card{
+  margin-left: 50px;
+  margin-right: 50px;
+  margin-top: 30px;
+}
 </style>
